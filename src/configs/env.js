9ae6143d145ac8ab9ac479872571afe0b0edc96b
adiisde <adityaa.sde@gmail.com>
2026-02-14 23:58:39 +0530

    chore: remove files accidentally added by another user during initial setup

--- src/configs/env.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config({ quiet: true });
-
-export const env = {
-  PORT: process.env.SR_PORT || 8000,
-  SUPABASE_URL: process.env.SUPABASE_URL,
-  SUPABASE_DB_URL: process.env.SUPABASE_DB_URL,
-  SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
-};
